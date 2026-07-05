9e3b03cb9b2dabc9d3c2174ff23baa33cb5b21f5
Vidhika Vilas Dhurve <vidhikavdhurve.27@gmail.com>
2026-07-05 15:36:51 +0530

    Feat: Add Sol chord substitute when two or more buttons are pressed together.

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,4 +1,4 @@
-// Pin Definitions
+// Pin Configuration
 const int BTN_DO = 2;
 const int BTN_RE = 3;
 const int BTN_MI = 4;
@@ -10,6 +10,7 @@ const int NOTE_DO = 262;
 const int NOTE_RE = 294;
 const int NOTE_MI = 330;
 const int NOTE_FA = 349;
+const int NOTE_SOL = 392; // Chord substitute note
 
 void setup() {
   pinMode(BTN_DO, INPUT_PULLUP);
@@ -20,21 +21,23 @@ void setup() {
 }
 
 void loop() {
-  // Read button states (LOW means pressed because of INPUT_PULLUP)
   bool pressDo = (digitalRead(BTN_DO) == LOW);
   bool pressRe = (digitalRead(BTN_RE) == LOW);
   bool pressMi = (digitalRead(BTN_MI) == LOW);
   bool pressFa = (digitalRead(BTN_FA) == LOW);
 
-  // Play highest priority single note, otherwise silence
-  if (pressDo) {
-    tone(BUZZER_PIN, NOTE_DO);
-  } else if (pressRe) {
-    tone(BUZZER_PIN, NOTE_RE);
-  } else if (pressMi) {
-    tone(BUZZER_PIN, NOTE_MI);
-  } else if (pressFa) {
-    tone(BUZZER_PIN, NOTE_FA);
+  // Count how many keys are pressed simultaneously
+  int pressedCount = pressDo + pressRe + pressMi + pressFa;
+
+  if (pressedCount >= 2) {
+    // Play Sol if two or more buttons are held together
+    tone(BUZZER_PIN, NOTE_SOL);
+  } else if (pressedCount == 1) {
+    // Single notes playing logic
+    if (pressDo) tone(BUZZER_PIN, NOTE_DO);
+    else if (pressRe) tone(BUZZER_PIN, NOTE_RE);
+    else if (pressMi) tone(BUZZER_PIN, NOTE_MI);
+    else if (pressFa) tone(BUZZER_PIN, NOTE_FA);
   } else {
     noTone(BUZZER_PIN);
   }
